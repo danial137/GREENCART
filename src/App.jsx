90abb5f4b379444bf905132c85bd5b3e0eb2e354
danial137 <danial79fakhrabadi@gmail.com>
2025-07-16 00:26:45 +0330

    Add Login component and integrate it into App for user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import Login from './components/Login'
 import { Toaster } from "react-hot-toast"
 import { Route, Routes, useLocation } from 'react-router-dom'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
 
 
   const isSellerPath = useLocation().pathname.includes('/seller')
-
+  const { showUserLogin } = useAppContext()
   return (
     <div>
 
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster />
       <div className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
         <Routes>
