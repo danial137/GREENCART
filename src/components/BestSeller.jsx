05236e0c7e0e84bee682e539d2587bbc70ade094
danial137 <danial79fakhrabadi@gmail.com>
2025-07-15 17:24:27 +0330

    Refactor BestSeller component layout and implement ProductCard component with dynamic product data

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import ProductCard from './ProductCard'
 const BestSeller = () => {
   return (
-    <div>BestSeller</div>
+      <div className='mt-16'>
+          <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
+          <div>
+              <ProductCard />
+          </div>
+    </div>
   )
 }
 
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext = createContext()
 
@@ -7,20 +8,37 @@ export const AppContext = createContext()
 export const AppContextProvider = ({ children }) => {
 
 
+    const currency = import.meta.VITE_CURRENCY
+
 
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
+
+
+    const fetchProducts = async () => {
+        setProducts(dummyProducts)
+    };
+
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
     const value = {
         navigate,
         user,
         setUser,
-        setIsSeller,
         isSeller,
+        setIsSeller,
         showUserLogin,
-        setShowUserLogin
+        setShowUserLogin,
+        products,
+        currency
+        
     }
     return (
         <AppContext.Provider value={value}>
