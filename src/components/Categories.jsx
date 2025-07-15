7a6a7ac48c4a1b98355faee076b51fc53b880f0d
danial137 <danial79fakhrabadi@gmail.com>
2025-07-15 03:39:50 +0330

    feat: update Categories component to dynamically render category items from assets

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { categories } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Categories = () => {
+    const { navigate } = useAppContext()
     return (
         <div className='mt-16 '>
             <p className='text-2xl md:text-3xl font-medium'>Categories</p>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-6 mt-6 xl:grid-cols-7'>
-              
-                <div className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center '>
-                    <img className='group-hover:scale-108 transition max-w-28' src={assets.box_icon} alt="" />
-                    <p>fruit</p>
-                </div>
+
+
+                {categories.map((category, index) => (
+                    <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'
+                        style={{ backgroundColor: category.bgColor }} onClick={() => {
+                            navigate(`/products/${category.path.toLowerCase()}`);
+                            scrollTo(0,0);
+
+                        }}>
+                        <img className='group-hover:scale-108 transition max-w-28' src={category.image} alt={category.text} />
+                        <p className='text-sm font-medium '>{category.text}</p>
+                    </div>
+                ))}
+
+
 
             </div>
         </div>
