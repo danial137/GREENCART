6421460420d467fa25fe2f068e016c7bb9443983
danial137 <danial79fakhrabadi@gmail.com>
2025-07-16 00:38:03 +0330

    Update BottomBanner component to use correct small banner image for mobile display

--- src/components/BottomBanner.jsx
+++ src/components/BottomBanner.jsx
@@ -5,9 +5,9 @@ const BottomBanner = () => {
     return (
         <div className='relative mt-24'>
             <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block' />
-            <img src={assets.bottom_banner_image} alt="banner" className='w-full md:hidden ' />
+            <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden ' />
 
-            <div className='absolute inset-0 flex flex-col md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 items-center justify-center'>
+            <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
                 <div>
                     <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are The Best?</h1>
                     {features.map((feature, index) => (
