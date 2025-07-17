7d3b9b685fe01878073aaff27c3841c34af7d6ca
danial137 <danial79fakhrabadi@gmail.com>
2025-07-18 03:15:25 +0330

    add orders productslist and seller layout component

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import { useAppContext } from './context/AppContext'
 import AddAddress from './pages/AddAddress'
 import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 const App = () => {
 
@@ -20,7 +24,7 @@ const App = () => {
   const isSellerPath = useLocation().pathname.includes('/seller')
   const { showUserLogin, isSeller } = useAppContext()
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
 
       {isSellerPath ? null : <Navbar />}
       {showUserLogin ? <Login /> : null}
@@ -34,7 +38,11 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
-          <Route path='/seller' element={isSeller ? null : <SellerLogin />}></Route>
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
+          </Route>
         </Routes>
       </div>
 
