3625aa3599c090e707ae26ba9c816baebc5b42b5
danial137 <danial79fakhrabadi@gmail.com>
2025-07-18 17:50:34 +0330

    customize index.css and addproduct

--- src/pages/seller/AddProduct.jsx
+++ src/pages/seller/AddProduct.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 const AddProduct = () => {
+
+
+
+    const [files, setFiles] = useState([])
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [category, setCategory] = useState('')
+    const [price, setPrice] = useState('')
+    const [offerPrice,setOfferPrice] = useState('')
+
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <div className="py-10 flex flex-col justify-between bg-white">
-            <form className="md:p-10 p-4 space-y-5 max-w-lg">
+        <div className="no-scrollbar flex-1 h-[95vh] overflow-y-hidden flex flex-col justify-between ">
+            <form onSubmit={onSubmitHandler} className="md:p-10 p-4 space-y-5 max-w-lg">
                 <div>
                     <p className="text-base font-medium">Product Image</p>
                     <div className="flex flex-wrap items-center gap-3 mt-2">
                         {Array(4).fill('').map((_, index) => (
                             <label key={index} htmlFor={`image${index}`}>
                                 <input accept="image/*" type="file" id={`image${index}`} hidden />
-                                <img className="max-w-24 cursor-pointer" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png" alt="uploadArea" width={100} height={100} />
+                              
                             </label>
                         ))}
                     </div>
